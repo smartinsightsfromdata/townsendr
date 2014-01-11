6f10156d5d854b8490aba62e29cc911ee0a26c49
philmikejones <philmikejones+github@gmail.com>
2014-01-11 21:05:25 +0000

    Density plots
    Added code for density plots

---
+++ source.R
@@ -0,0 +1,26 @@
+# Purpose ====
+
+# This R script is to prepare the Census data:
+# 1. Check and if necessary transform the data so that it can be used
+# 2. Convert the four domains in to z-scores so they can be combined
+# 3. Combine the z-scores to form one overall index
+
+# License ====
+
+# GPL v2. See LICENSE.md
+
+# Libraries ====
+
+library(ggplot2)
+
+# Prepare Data ====
+
+overcrowd.frame <- Data_personsperroom_englandwales_LADs
+tenure.frame    <- Data_tenure_lad
+unemp.frame     <- Data_unemployed_economicallyactive_engwal_LADs
+car.frame       <- Data_CARVAN_UNIT
+
+ggplot(overcrowd.frame, aes(overcrowd.frame$pcGt1PPerRoom)) + geom_density()
+ggplot(tenure.frame, aes(tenure.frame$pcNotOO)) + geom_density()
+ggplot(unemp.frame, aes(unemp.frame$pcEconActUnem)) + geom_density()
+ggplot(car.frame, aes(car.frame$pcNoCar)) + geom_density()

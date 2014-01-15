1207024d7ee88f510da60b8aed23b5df8415eec5
philmikejones <philmikejones+github@gmail.com>
2014-01-15 21:00:58 +0000

    columns to data frames

--- source.R
+++ source.R
@@ -3,6 +3,10 @@
 library(ggplot2)
 library(gridExtra) # For panelling ggplots
 
+# Read data frames ====
+
+
+
 # Prepare Data ====
 
 overcrowd.frame <- Data_personsperroom_englandwales_LADs
@@ -150,10 +154,14 @@ rm(plot1, plot2, plot3, plot4)
 
 # Z-scores ====
 
-zOvercrowd <- scale(logOvercrowding, center = T, scale = T)
-zTenure    <- scale(logTenure, center = T, scale = T)
-zUnemp     <- scale(unemp.frame$pcEconActUnem, center = T, scale = T)
-zCar       <- scale(logCarOwn, center = T, scale = T)
+Data_personsperroom_englandwales_LADs$zOvercrowd <- 
+  scale(logOvercrowding, center = T, scale = T)
+Data_tenure_lad$zTenure <- 
+  scale(logTenure, center = T, scale = T)
+Data_unemployed_economicallyactive_engwal_LADs$zUnemp <- 
+  scale(unemp.frame$pcEconActUnem, center = T, scale = T)
+Data_CARVAN_UNIT$zCar <- 
+  scale(logCarOwn, center = T, scale = T)
 
 merged <- merge(overcrowd.frame, tenure.frame, by.x = "GEOCODE", by.y = "GEOCODE")
 merged <- merge(merged, unemp.frame, by.x = "GEOCODE", by.y = "GEOCODE")

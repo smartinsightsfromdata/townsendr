a8dd8032af2465012d487d50fcee0d4b1bb77376
philmikejones <philmikejones+github@gmail.com>
2014-02-21 11:58:44 +0000

    changed source to nomis web; removed plots
    Nomis web doesn’t require registration like UK Data Service, so I’ve
    updated the source to use Nomis Web Census tables instead

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 Introduction
 ===============
 
-The Townsend Material Deprivation score was originally devised by Townsend et al (1988). It is based on the following four Census variables:
+The Townsend Material Deprivation score was originally devised by Townsend et al
+(1988). It is based on the following four Census variables:
 
-1. Percent of households without access to a car.
-2. Percent of households with more than one person per room.
-3. Percent of households not owner-occupied.
-4. Percent of individuals who are economically active unemployed (i.e. could work).
+	1. Percent of households without access to a car.
+	2. Percent of households with more than one person per room.
+	3. Percent of households not owner-occupied.
+	4. Percent of individuals who are economically active unemployed.
 
-The four domains above are then combined to produce the overall Townsend Score. This score can be calculated for any geographical area that is available in the Census tables, for example LSOA, MSOA, Ward, LAD, etc.
+The four domains above are then combined to produce the overall Townsend Score.
+This score can be calculated for any geographical area that is available in the
+Census tables, for example LSOA, MSOA, Ward, LAD, etc.
 
-Because of the way the score is combined, only the relative rank of the scores is meaningful. In practice, this means it is possible to say an area is more or less deprived than another, but not what ‘amount’ of deprivation it experiences. For this reason it is common to create a score based on a national dataset. This script uses data from England and Wales, as Scotland and Northern Ireland tables were not available at the time of publication (February 2014). Thus, the scores reflect the relative deprivation based on England and Wales.
+Because of the way the score is combined, only the relative rank of the scores
+is meaningful. In practice, this means it is possible to say an area is more or
+less deprived than another, but not what ‘amount’ of deprivation it experiences.
+For this reason it is common to create a score based on a national dataset. This
+script uses data from England and Wales, as Scotland and Northern Ireland tables
+were not available at the time of publication (February 2014). Thus, the scores
+reflect the relative deprivation based on England and Wales.
 
 Purpose
 ===============
@@ -26,14 +35,16 @@ Using the Script
 
 Copy the script (source.R) to your system.
 
-You need four data tables, one for each domain of deprivation, at the geographical level you’re interested in (LSOA, Ward, etc.). Names these:
+You need four data tables, one for each domain of deprivation, at the geographical level you’re interested in (LSOA, Ward, etc.). They’ll probably 
+all be called ‘bulk.csv’ when you download them, so rename these:
 
 1. car.csv
-2. overcrowd.csv
+2. oc.csv (overcrowd)
 3. tenure.csv
 4. unemp.csv
 
-I’ve provided four example files from Nomis Web at the LAD level, which you can use for testing or if this is the geography you want. Additional tables are available from: http://www.nomisweb.co.uk/census/2011/data_finder
+I’ve provided four example files from Nomis Web at the LAD (district/unitary) 
+level, which you can use for testing or if this is the geography you want. Additional tables are available from: http://www.nomisweb.co.uk/census/2011/data_finder
 
 Place the four data files in the same folder as source.R.
 
@@ -49,10 +60,16 @@ Contact
 ===============
 phil dot jones at sheffield dot ac dot uk (no spaces)
 
+Thanks and acknowledgements
+===========================
+
+Thanks to Robin Lovelace - https://github.com/Robinlovelace - for having
+a once-over of the script and his suggestions.
+
 References
 ===============
 
-Office for National Statistics, 2011 Census: Aggregate data (England and Wales) [computer file]. UK Data Service Census Support. Downloaded from: http://infuse.mimas.ac.uk. This information is licensed under the terms of the Open Government Licence [http://www.nationalarchives.gov.uk/doc/open-government-licence/version/2].
+Office for National Statistics, 2011 Census: Aggregate data (England and Wales) [computer file]. Downloaded from: http://www.nomisweb.co.uk/census/2011 [Accessed February 2014]. This information is licensed under the terms of the Open Government Licence [http://www.nationalarchives.gov.uk/doc/open-government-licence/version/2].
 
 Office for National Statistics, 2011 Census: Digitised Boundary Data (England and Wales) [computer file]. UK Data Service Census Support. Downloaded from: http://edina.ac.uk/census
 
--- source.R
+++ source.R
@@ -1,30 +1,20 @@
-
-
 # License ====
 
 # Calculate Townsend Material Deprivation Score from 2011 Census Data in the UK
 # Copyright (C) 2014 Phil Mike Jones - orcid.org/0000-0001-5173-3245
   
-# This programme is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# This programme is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the 
+# Free Software Foundation, either version 3 of the License, or 
 # (at your option) any later version.
 
-# This programme is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# This programme is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+# for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with this programme. If not, see <http://www.gnu.org/licenses/>.
-
-
-
-# Thanks and acknowledgements ====
-
-# Thanks to Robin Lovelace - https://github.com/Robinlovelace - for having
-# a once-over of the script and his suggestions.
-# (Dimitris: he did this after I handed it in for assessment!)
+# You should have received a copy of the GNU General Public License along with
+# this programme. If not, see <http://www.gnu.org/licenses/>.
 
 
 
@@ -39,73 +29,78 @@ library(gridExtra) # For panelling ggplots
 
 # Read in files for car access, persons per room, tenure, and unemp
 # These can be at any geography desired (e.g. LSOA, MSOA, ward, LAD, etc)
+# These can be obtained from Nomis Web's Census table finder:
+# http://www.nomisweb.co.uk/census/2011/data_finder
 
 # Car Access
-car         <- read.csv("Data_car_engwal_lsoa.csv",
-                 header = F,
-                 skip = 2)
-keep        <- c("V2", "V6", "V7")
-car         <- car[keep]
+car <- read.csv("car.csv", header = T)
+car <- subset(car, Rural.Urban == "Total")
+car <- as.data.frame(car)
+car$yesCar <- rowSums(car[, 7:10])
+colnames(car)[5] <- "allHh"
+colnames(car)[6] <- "noCar"
+car$pcNoCar <- (car$noCar / car$allHh) * 100
+keep <- c("geography.code", "allHh", "noCar", "yesCar", "pcNoCar")
+car <- car[keep]
 rm(keep)
-names(car)  <- c("lsoacode", "allHh", "noCar")
-car$pcNoCar <- 100 * (car$noCar / car$allHh)
-keep        <- c("lsoacode", "pcNoCar")
-car         <- car[keep]
+View(car)
+
+# Overcrowding (more than one person per room (NOT bedroom!))
+oc <- read.csv("oc.csv", header = T)
+oc <- subset(oc, Rural.Urban == "Total")
+oc <- as.data.frame(oc)
+oc$yesOc <- rowSums(oc[, 8:9])
+oc$noOc  <- rowSums(oc[, 6:7])
+colnames(oc)[5] <- "allHh"
+oc$pcYesOc <- (oc$yesOc / oc$allHh) * 100
+keep <- c("geography.code", "allHh", "noOc", "yesOc", "pcYesOc")
+oc <- oc[keep]
 rm(keep)
 
-# Overcrowding (more than one person per room)
-overcrowd         <- read.csv("Data_personperroom_engwal_lsoa.csv",
-                      header = F,
-                      skip = 2)
-keep              <- c("V2", "V6", "V9", "V10")
-overcrowd         <- overcrowd[keep]
+# Tenure (not owner-occupied. Shared ownership not included in O-O)
+tenure <- read.csv("tenure.csv", header = T)
+tenure <- subset(tenure, Rural.Urban == "Total")
+tenure <- as.data.frame(tenure)
+colnames(tenure)[5] <- "allHh"
+colnames(tenure)[6] <- "yesOo" # Does not include shared ownership
+keep <- c("geography.code", "allHh", "yesOo", "Tenure..Shared.ownership..part.owned.and.part.rented...measures..Value", "Tenure..Social.rented..measures..Value", "Tenure..Private.rented..measures..Value", "Tenure..Living.rent.free..measures..Value")
+tenure <- tenure[keep]
 rm(keep)
-names(overcrowd)  <- c("lsoacode", "allHh", "ppr1", "ppr15")
-overcrowd$yesOc   <- rowSums(overcrowd[, 3:4])
-overcrowd$pcYesOc <- 100 * (overcrowd$yesOc / overcrowd$allHh)
-keep              <- c("lsoacode", "pcYesOc")
-overcrowd         <- overcrowd[keep]
+tenure$noOo <- rowSums(tenure[, 4:7])
+keep <- c("geography.code", "allHh", "yesOo", "noOo")
+tenure <- tenure[keep]
+rm(keep)
+tenure$pcNoOo <- (tenure$noOo / tenure$allHh) * 100
+
+# Economically active unemployed (Census table QS601EW)
+unemp <- read.csv("unemp.csv", header = T)
+unemp <- subset(unemp, Rural.Urban == "Total")
+unemp <- as.data.frame(unemp)
+# Use all econ active residents, NOT all persons (see Townsend 1988: 36)
+colnames(unemp)[6] <- "allEconAct"
+colnames(unemp)[13] <- "yesUnemp"
+unemp$pcUnemp <- (unemp$yesUnemp / unemp$allEconAct) * 100
+keep <- c("geography.code", "allEconAct", "yesUnemp", "pcUnemp")
+unemp <- unemp[keep]
 rm(keep)
 
-# Tenure (not owner-occupied)
-tenure         <- read.csv("Data_tenure_engwal_lsoa.csv",
-                              header = F,
-                              skip = 2)
-keep           <- c("V2", "V6", "V11", "V12", "V13")
-tenure         <- tenure[keep]
-rm(keep)
-names(tenure)  <- c("lsoacode", "allHh", "socRent", "prvRent", "rentFree")
-tenure$notOo   <- rowSums(tenure[, 3:5])
-tenure$pcNotOo <- 100 * (tenure$notOo / tenure$allHh)
-keep           <- c("lsoacode", "pcNotOo")
-tenure         <- tenure[keep]
-rm(keep)
 
-# Economically active unemployed
-unemp     <- read.csv("Data_unemp_engwal_lsoa.csv",
-                      header = F,
-                      skip = 2)
-keep           <- c("V2", "V6", "V7")
-unemp         <- unemp[keep]
-rm(keep)
-names(unemp)  <- c("lsoacode", "unempEa", "allPers")
-unemp$pcUnemp <- 100 * (unemp$unempEa / unemp$allPers)
-keep           <- c("lsoacode", "pcUnemp")
-unemp         <- unemp[keep]
-rm(keep)
 
-# Merge file
-master <- merge(car, overcrowd, by = "lsoacode")
-master <- merge(master, tenure, by = "lsoacode")
-master <- merge(master, unemp,  by = "lsoacode")
-View(master)
+# Create Master File ====
+
+master <- merge(car, oc, by = "geography.code")
+master <- merge(master, tenure, by = "geography.code")
+master <- merge(master, unemp,  by = "geography.code")
+
+
 
 # Check distributions ====
 
 # By default the code to check the distributions is commented out.
-# Z-scores can be created from the original distribution, even if it's not ideal
-# Townsend et al (1988) did transform some of the variables.
-# The code is provided to check the distributions if you want to transform.
+# Z-scores don't assume a normal distribution.
+# Townsend et al (1988) transformed unemployment and overcrowding (p. 165).
+# This block is therefore provided if you want to check and transform.
+# It will output a pdf - "check-distributions.pdf" - for inspection.
 
 # require(gridExtra)
 # # Plot each varaible as density and qq plot. Linear = good
@@ -131,14 +126,14 @@ View(master)
 #   stat_qq() + 
 #   ggtitle("QQ: overcrowd")
 # 
-# plot5 <- ggplot(master, aes(master$pcNotOo)) +
+# plot5 <- ggplot(master, aes(master$pcNoOo)) +
 #   geom_density() +
 #   ggtitle("Percent of households not owner-occupied") +
 #   xlab("Density: tenure") +
 #   stat_function(fun = dnorm, 
-#                 args = list(mean = mean(master$pcNotOo, na.rm = T), 
-#                             sd = sd(master$pcNotOo, na.rm = T)))
-# plot6 <- ggplot(master, aes(sample = master$pcNotOo)) + 
+#                 args = list(mean = mean(master$pcNoOo, na.rm = T), 
+#                             sd = sd(master$pcNoOo, na.rm = T)))
+# plot6 <- ggplot(master, aes(sample = master$pcNoOo)) + 
 #   stat_qq() + 
 #   ggtitle("QQ: tenure")
 # 
@@ -160,6 +155,9 @@ View(master)
 # dev.off()
 # rm(plot1, plot2, plot3, plot4, plot5, plot6, plot7, plot8)
 
+# If you want to transform any of the variables, you could do something like:
+# master$logCar <- log(master$pcNoCar)
+
 
 
 # Z-scores ====
@@ -168,15 +166,14 @@ View(master)
 
 master$zCar       <- scale(master$pcNoCar, center = T, scale = T)
 master$zOvercrowd <- scale(master$pcYesOc, center = T, scale = T)
-master$zTenure    <- scale(master$pcNotOo, center = T, scale = T)
+master$zTenure    <- scale(master$pcNoOo, center = T, scale = T)
 master$zUnemp     <- scale(master$pcUnemp, center = T, scale = T)
-
 
 # Combine z-scores in to one score
 master$z <- rowSums(master[c("zCar", "zOvercrowd", "zTenure", "zUnemp")])
 
 # Drop unnecessary colums
-keep   <- c("lsoacode", "z")
+keep   <- c("geography.code", "z")
 master <- master[keep]
 rm(keep)
 
